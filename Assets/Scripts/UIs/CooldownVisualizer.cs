20093d7d09e5db1240d1ad70ac612f201131e99e
WagaTonoka <hdh060209@gmail.com>
2026-02-26 05:13:34 +0900

    feat: UICooldownChecker renamed to COoldownVisualizer and timer is created in CooldownVisualizer

---
+++ Assets/Scripts/UIs/CooldownVisualizer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CooldownVisualizer : MonoBehaviour
+{
+    [Header("필수 사전 할당")]
+    [SerializeField] private Image COOLDOWN_IMAGE; // 쿨타임 시계 그거
+
+    [Header("쿨다운 수치 사전 설정")]
+    private float _totalCooldownTime = 5.0f;
+    private bool _isCooldown = false;
+
+    private Coroutine _cooldownAnimeCoroutine;
+
+    // 사전 초기화가 필요할 때 사용.
+    // 아직 구현은 없는데, 만약 스크립터블로 스킬을 관리한다면,
+    // 그때, 그거 받아오게 구현하면 될 듯.
+    public void Initialize()
+    {
+        
+    }
+
+    // 쿨다운 도는 애니메이션 실행.
+    // 총 쿨다운 시간을 넣어도 되고, 안 넣어도 됨.
+    // 안넣으면 사전 설정 값을 가져옴.
+    public void Play(float? cooldownTime)
+    {
+        
+    }
+
+    // 쿨다운 도는 애니메이션을 처음으로 초기화.
+    // 애니메이션 상태만 처음으로 즉시 변경.
+    public void Reset()
+    {
+        
+    }
+
+    IEnumerator StartCooldown()
+    {
+        _isCooldown = true;
+        COOLDOWN_IMAGE.fillAmount = 1; // 쿨타임 시작, 이미지 가득 채움
+
+        float timer = 0;
+        while (timer < _totalCooldownTime)
+        {
+            timer += Time.deltaTime;
+            COOLDOWN_IMAGE.fillAmount = 1 - (timer / _totalCooldownTime); // 1에서 0으로 감소
+            yield return null;
+        }
+
+        COOLDOWN_IMAGE.fillAmount = 0;
+        _isCooldown = false;
+    }
+}
