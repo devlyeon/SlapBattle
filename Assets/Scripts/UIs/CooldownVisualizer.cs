9ab4e849c9abe7a07cbb103b06392dff7ea51257
WagaTonoka <hdh060209@gmail.com>
2026-02-27 05:10:09 +0900

    feat: Prototype Icon Script is Created.

--- Assets/Scripts/UIs/CooldownVisualizer.cs
+++ Assets/Scripts/UIs/CooldownVisualizer.cs
@@ -1,55 +1,101 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
+using TMPro;
 
 public class CooldownVisualizer : MonoBehaviour
 {
     [Header("필수 사전 할당")]
-    [SerializeField] private Image COOLDOWN_IMAGE; // 쿨타임 시계 그거
+    [SerializeField] private Image COOLDOWN_COVER_IMAGE;
+    [SerializeField] private FeedbackVisualizer FEEDBACK_VISUALIZER;
+    [SerializeField] private TMP_Text COOLDOWN_TEXT;
+    [SerializeField] private CanvasGroup COOLDOWN_TEXT_CANVASGROUP;
+
+    [Header("유니티 이벤트 시스템")]
+    [SerializeField] private UnityEvent ON_PLAY_UNITYEVENT;
+    [SerializeField] private UnityEvent ON_COMPLETE_UNITYEVENT;
 
     [Header("쿨다운 수치 사전 설정")]
-    private float _totalCooldownTime = 5.0f;
-    private bool _isCooldown = false;
+    [SerializeField] private float _totalCooldownTime = 1.0f;
+    [SerializeField] private bool _isCooldown = false;
+
+    public float TotalCooldownTime => _totalCooldownTime;
+    public bool IsCooldown => _isCooldown;
 
     private Coroutine _cooldownAnimeCoroutine;
 
-    // 사전 초기화가 필요할 때 사용.
-    // 아직 구현은 없는데, 만약 스크립터블로 스킬을 관리한다면,
-    // 그때, 그거 받아오게 구현하면 될 듯.
+    private void Start()
+    {
+        this.Initialize();
+    }
+
+    // 사전 초기화가 필요할 때 사용
     public void Initialize()
     {
-        
+        COOLDOWN_COVER_IMAGE.fillAmount = 0f;
+        COOLDOWN_TEXT.text = _totalCooldownTime.ToString();
+        COOLDOWN_TEXT_CANVASGROUP.alpha = 0f;
+        _isCooldown = false;
     }
 
-    // 쿨다운 도는 애니메이션 실행.
-    // 총 쿨다운 시간을 넣어도 되고, 안 넣어도 됨.
-    // 안넣으면 사전 설정 값을 가져옴.
-    public void Play(float? cooldownTime)
+    // 쿨다운 연출을 정지
+    public void Stop()
     {
-        
+        if (_cooldownAnimeCoroutine != null)
+            StopCoroutine(_cooldownAnimeCoroutine);
     }
 
-    // 쿨다운 도는 애니메이션을 처음으로 초기화.
-    // 애니메이션 상태만 처음으로 즉시 변경.
+    // 쿨다운 연출을 초기화
     public void Reset()
     {
-        
+        this.Stop();
+        this.Initialize();
     }
 
-    IEnumerator StartCooldown()
+    // 쿨다운 연출을 재생
+    public void Play()
     {
-        _isCooldown = true;
-        COOLDOWN_IMAGE.fillAmount = 1; // 쿨타임 시작, 이미지 가득 채움
+        if (_isCooldown)
+            this.Reset();
+        _cooldownAnimeCoroutine = StartCoroutine(CooldownAnime());
+    }
 
+    // 쿨다운 도는 애니메이션
+    private IEnumerator CooldownAnime()
+    {
+        // 현출 시작
+        _isCooldown = true;
+        ON_PLAY_UNITYEVENT.Invoke();
+        COOLDOWN_COVER_IMAGE.fillAmount = 1f;
+        COOLDOWN_TEXT_CANVASGROUP.alpha = 1f;
+
+        // 쿨타임 돌리기
         float timer = 0;
         while (timer < _totalCooldownTime)
         {
             timer += Time.deltaTime;
-            COOLDOWN_IMAGE.fillAmount = 1 - (timer / _totalCooldownTime); // 1에서 0으로 감소
+            COOLDOWN_COVER_IMAGE.fillAmount = 1 - (timer / _totalCooldownTime);
+
+            if ((int)_totalCooldownTime - timer >= 1f)
+                COOLDOWN_TEXT.text = ((int)(_totalCooldownTime - timer)).ToString();
+            else
+                COOLDOWN_TEXT.text = Math.Round(_totalCooldownTime - timer, 1).ToString();
+
             yield return null;
         }
 
-        COOLDOWN_IMAGE.fillAmount = 0;
+        // 연출 초기화
+        ON_COMPLETE_UNITYEVENT.Invoke();
+        COOLDOWN_COVER_IMAGE.fillAmount = 0f;
+        FEEDBACK_VISUALIZER.Play();
+        COOLDOWN_TEXT_CANVASGROUP.alpha = 0f;
         _isCooldown = false;
+        this.Reset();
+        yield return new WaitForSeconds(FEEDBACK_VISUALIZER.TotalFeedbackTime);
+
+        FEEDBACK_VISUALIZER.Reset();
+        yield return null;
     }
 }
