eb8eea0c0086c957bdef08fde5d4082fc640e724
WagaTonoka <hdh060209@gmail.com>
2026-02-27 16:41:50 +0900

    feat: HP Bar Anime is Created

---
+++ Assets/Scripts/UIs/HP/DelayedHealthBar.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+
+public class DelayedHealthBar : HealthBarBase
+{
+    [Header("수치 사전 설정")]
+    [SerializeField] private float _delayedTime = 0.25f;
+    [SerializeField] private float _delayingSpeed = 0.25f;
+
+    private Coroutine _valueChangeAnimeCoroutine;
+
+    private void Awake()
+    {
+        if (_isExclusive)
+            this.Initialize(null);
+    }
+
+    /// <summary>
+    /// HP 바에 수치를 바로 설정하는 함수
+    /// </summary>
+    /// <param name="hpValue">설정하고 싶은 HP 값 (음수 X)</param>
+    public override void SetHpValue(float hpValue)
+    {
+        if (!CanExecute())
+            return;
+
+        if (hpValue < 0f)
+        {
+            Debug.LogError($"{_className}: hpValue는 음수가 될 수 없습니다!");
+            return;
+        }
+
+        if (_valueChangeAnimeCoroutine != null)
+            StopCoroutine(_valueChangeAnimeCoroutine);
+
+        _valueChangeAnimeCoroutine = StartCoroutine(ValueChangeAnime(hpValue));
+    }
+
+    private IEnumerator ValueChangeAnime(float hpValue)
+    {
+        // 줄어든 수치 피드백용 딜레이
+        yield return new WaitForSeconds(_delayedTime);
+
+        float initialHp = _currentHP;
+        float resultHP = hpValue;
+
+        // 딜레이 연출 시작
+        float timer = 0;
+        while (timer < _delayingSpeed)
+        {
+            timer += Time.deltaTime;
+
+            _currentHP = Mathf.Lerp(
+                    initialHp,
+                    resultHP,
+                    timer / _delayingSpeed);
+
+            HP_BAR_IMAGE.fillAmount = NormalizeToRange(_currentHP);
+            yield return null;
+        }
+
+        // 마지막 값 보정
+        _currentHP = resultHP;
+        HP_BAR_IMAGE.fillAmount = NormalizeToRange(_currentHP);
+    }
+}
---
+++ Assets/Scripts/UIs/HP/InstantHelathBar.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class InstantHelathBar : HealthBarBase
+{
+    private void Awake()
+    {
+        if (_isExclusive)
+            this.Initialize(null);
+    }
+
+    /// <summary>
+    /// HP 바에 수치를 바로 설정하는 함수
+    /// </summary>
+    /// <param name="hpValue">설정하고 싶은 HP 값 (음수 X)</param>
+    public override void SetHpValue(float hpValue)
+    {
+        if (!CanExecute())
+            return;
+        
+        if (hpValue < 0f)
+        {
+            Debug.LogError($"{_className}: hpValue는 음수가 될 수 없습니다!");
+            return;
+        }
+
+        _currentHP = hpValue;
+        HP_BAR_IMAGE.fillAmount = NormalizeToRange(_currentHP);
+    }
+}
