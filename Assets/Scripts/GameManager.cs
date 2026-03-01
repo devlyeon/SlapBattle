5108006248031c956bc7c41a32cbc1709144e0e8
Lyeon <devlyeon@gmail.com>
2026-03-01 10:02:02 +0900

    Add stat animation on intro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,14 @@ public class GameManager : MonoBehaviour
 {    
     public static bool gameStarted = false;
 
+    [SerializeField] private Animator animatorA, animatorB;
+
+    public void BeforeStart()
+    {
+        animatorA.SetTrigger("In");
+        animatorB.SetTrigger("In");
+    }
+
     public void StartGame()
     {
         gameStarted = true;
