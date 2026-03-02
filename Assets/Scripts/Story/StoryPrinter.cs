b4074de0b836504d07020d0f9891cc1fdfca8471
Lyeon <devlyeon@gmail.com>
2026-03-02 17:36:46 +0900

    fix style, story

--- Assets/Scripts/Story/StoryPrinter.cs
+++ Assets/Scripts/Story/StoryPrinter.cs
@@ -17,7 +17,7 @@ public class StoryPrinter : MonoBehaviour
 
     [SerializeField] private List<string> data = new List<string>();
     private int currentPosition = 0;
-    private bool isPrinting = false, isFinish = false;
+    private bool isPrinting = false, isFinish = false, isDelay = false;
     [SerializeField] private Animator animator;
 
     void Start()
@@ -45,6 +45,7 @@ public class StoryPrinter : MonoBehaviour
     {
         animator.SetBool("Show", false);
         yield return new WaitForSeconds(1.0f);
+        animator.Rebind();
         gameObject.SetActive(false);
     }
 
@@ -58,14 +59,33 @@ public class StoryPrinter : MonoBehaviour
     {
         if (!isPrinting)
         { // 만약 출력 중이 아니라면
-            PrintText(); // 다음 대사가 있다면 출력합니다.
+            if (currentPosition == 6)
+            {
+                scriptText.text = "";
+                dpText.gameObject.SetActive(true);
+                animator.SetTrigger("DP");
+                StartCoroutine(DPlusAnimDelay());
+            }
+            else
+            {
+                PrintText(); // 다음 대사가 있다면 출력합니다.
+            }
         }
         else
         { // 출력 중이라면
+            if (currentPosition == 6 && isDelay) return;
             StopCoroutine(coroutine); // 출력 중인 코루틴을 멈추고
             scriptText.text = data[currentPosition - 1]; // 대사는 한 번에 전부 보여줍니다.
             isPrinting = false; // 텍스트가 출력 중이 아님을 표시합니다.
         }
+    }
+
+    IEnumerator DPlusAnimDelay()
+    {
+        isDelay = true;
+        yield return new WaitForSeconds(1.5f);
+        PrintText();
+        isDelay = false;
     }
 
     void PrintText()
@@ -79,11 +99,6 @@ public class StoryPrinter : MonoBehaviour
         }
 
         coroutine = StartCoroutine(PrintText(data[currentPosition]));
-        if (currentPosition == 6)
-        {
-            dpText.gameObject.SetActive(true);
-            animator.SetTrigger("DP");
-        }
         if (++currentPosition == data.Count) isFinish = true;
     }
 
@@ -92,6 +107,7 @@ public class StoryPrinter : MonoBehaviour
         isPrinting = true; // 출력 중임을 알립니다.
         for (int i = 1; i <= script.Length; i++)
         { // 대사의 글자 수만큼 반복합니다.
+            if (script.Substring(i - 1, 1) == "\\") i++;
             scriptText.text = script.Substring(0, i); // 대사를 한 글자씩 나타나게끔 출력합니다.
             yield return new WaitForSeconds(0.02f);
         }
